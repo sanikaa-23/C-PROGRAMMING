a62bd8a434400f78dc24e78aeaed5ef98a7f575e
sanikaa-23 <sanika.gg23@gmail.com>
2026-04-21 00:33:50 +0530

    Add swap function to demonstrate value swapping

---
+++ Assignment20.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+void swap(int a, int b) {
+
+    int temp = a;
+
+    a = b;
+
+    b = temp;
+
+    printf("Inside function: a=%d b=%d\n", a, b);
+
+}
+
+
+
+int main() {
+
+    int x = 10, y = 20;
+
+    swap(x, y);
+
+    printf("Outside function: x=%d y=%d\n", x, y);
+
+    return 0;
+
+
+}
+
